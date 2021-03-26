fab0eef3983191ed4c81ec848f51db450e09abc7
sh4dow-author <sh4dowauthor@gmail.com>
2021-03-27 00:09:30 +0300

    added getters, setters for width and height. Rewrited preprocess logic

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,13 +1,33 @@
-import base64
+from base64 import decodestring
+from PIL import Image
 import numpy as np
-import cv2
 
 
 class Preprocessor:
-    @staticmethod
-    def preprocess(img_bytes_string):
-        image_base64_decode = base64.decodebytes(img_bytes_string)
-        arr_encode = np.fromstring(image_base64_decode, np.uint8)
-        preprocessed_image = cv2.imdecode(buf=arr_encode, flags=cv2.IMREAD_GRAYSCALE)
+    __width = 224
+    __height = 224
 
-        return preprocessed_image
+    def preprocess(self, img_bytes_string):
+        image = Image.fromstring('RGB', (self.__height, self.__width), decodestring(img_bytes_string))
+        array_image = np.array(image).astype(np.float32)
+        array_image = np.expand_dims(array_image, axis=0)
+        array_image /= 255.
+        return array_image
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value > 0:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value > 0:
+            self.__height = value
