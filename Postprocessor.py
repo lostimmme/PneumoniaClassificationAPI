d404e98dc86dfb9db771c0ed90238c033414e7a7
sh4dow-author <sh4dowauthor@gmail.com>
2021-03-27 00:09:48 +0300

    writed postprocess logic

--- Postprocessor.py
+++ Postprocessor.py
@@ -2,5 +2,9 @@ import numpy as np
 
 
 class Postprocessor:
-    def postprocess(self):
-        pass
+    @staticmethod
+    def postprocess(result):
+        result = result[0][0]
+        result = round(result, 4)
+        result *= 100
+        return result
