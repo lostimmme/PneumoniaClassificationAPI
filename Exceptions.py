0fdecb1265f89aa711947aa3b06d4212c67a02c8
Павел Ярмолюк <1070111925@bntuby.onmicrosoft.com>
2021-04-14 20:16:05 +0300

    Rewrited logic of custom exceptions

--- Exceptions.py
+++ Exceptions.py
@@ -1,14 +1,20 @@
 class ModelArchitectureNotLoaded(Exception):
     """If model architecture not loaded and you are trying to set weights for None or use predict method for model
     this exception will be raised."""
-    pass
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
 
 
 class IncorrectImageSize(Exception):
     """If you are trying to set incorrect size to image this exception will be raised"""
-    pass
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
 
 
 class ModelFileNotFounded(Exception):
     """If file with model not founded this exception will be raised"""
-    pass
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
